821c239ca3d50bc61248c14ac6feee1d1a5f0cbd
Mike Allred <mikejallred@gmail.com>
2017-05-17 00:21:34 -0400

    Slowly climbing back from lost code.

--- base.py
+++ base.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
+import boto3
+from imgurpython import ImgurClient
 import inspect
 import os
+from PIL import Image 
+from PIL.ExifTags import TAGS
 import pprint
 import praw
 import tempfile
 import urllib.request
+
+imgur = ImgurClient(
+  os.environ['RMG_IMGUR_CLIENT_ID'],
+  os.environ['RMG_IMGUR_CLIENT_SECRET'],
+)
 
 pp = pprint.PrettyPrinter(indent=2)
 
@@ -12,3 +21,5 @@ reddit = praw.Reddit(
   user_agent='ratmongo'
 )
 subreddit = reddit.subreddit('ratmongo')
+
+rekog = boto3.client('rekognition')
--- listen.py
+++ listen.py
@@ -13,9 +13,17 @@ def retrieve(url):
   )
   print(file_temp.name)
   urllib.request.urlretrieve(url, file_temp.name)
+  i = Image.open(file_temp.name)
+  info = i._getexif()
+  for tag, value in info.items():
+    decoded = TAGS.get(tag, tag)
+    #pp.pprint(decoded)
+    #pp.pprint(value)
+
+
 
 for submission in subreddit.stream.submissions():
-  #pp.pprint(inspect.getmembers(submission))
+  pp.pprint(inspect.getmembers(submission))
   if submission.preview:
     #pp.pprint(submission.preview['images'][0]['resolutions'][-1]['url'])
     url = submission.preview['images'][0]['resolutions'][-1]['url']
