80ed0f236c95e1db8eabff3389d55893c4f8f94b
Mike Allred <mikejallred@gmail.com>
2017-05-16 23:16:06 -0400

    Getting back to where it was.

--- base.py
+++ base.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
+import inspect
 import os
+import pprint
 import praw
+import tempfile
+import urllib.request
+
+pp = pprint.PrettyPrinter(indent=2)
 
 reddit = praw.Reddit(
   user_agent='ratmongo'
--- listen.py
+++ listen.py
@@ -4,5 +4,20 @@ from base import *
 #print(reddit.read_only)
 #print(subreddit.title)
 
+def retrieve(url):
+  file_temp = tempfile.NamedTemporaryFile(
+    delete=False,
+    mode='w+b',
+    prefix='rmg-',
+    suffix='.jpg',
+  )
+  print(file_temp.name)
+  urllib.request.urlretrieve(url, file_temp.name)
+
 for submission in subreddit.stream.submissions():
-  print(submission.title)
+  #pp.pprint(inspect.getmembers(submission))
+  if submission.preview:
+    #pp.pprint(submission.preview['images'][0]['resolutions'][-1]['url'])
+    url = submission.preview['images'][0]['resolutions'][-1]['url']
+    retrieve(url)
+    print(submission.title)
