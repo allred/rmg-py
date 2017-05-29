477c3ae731b5a03fd58c1baa0f3ff6c4a1f1943d
Mike Allred <mikejallred@gmail.com>
2017-05-28 20:20:58 -0400

    Download preview and original. Starting to deal with exif.

--- listen.py
+++ listen.py
@@ -4,36 +4,51 @@ from base import *
 #print(reddit.read_only)
 #print(subreddit.title)
 
-def retrieve(url):
-  file_temp = tempfile.NamedTemporaryFile(
-    delete=False,
-    mode='w+b',
-    prefix='rmg-',
-    suffix='.jpg',
-  )
-  print(file_temp.name)
-  #urllib.request.urlretrieve(url, file_temp.name)
-  #out = imgur.get_image(url)
-  #print(out)
-  print(url)
-  m = re.search('^.*\/(\S+?)$', url)
-  print({'grp': m.group(0)})
-  return
+def getexif(file_temp):
   i = Image.open(file_temp.name)
+  # FIXME: imgur strips exif, get exif from the smallest reddit thumbnail
   info = i._getexif()
-  for tag, value in info.items():
-    decoded = TAGS.get(tag, tag)
+  #pp.pprint(inspect.getmembers(info))
+  #for tag, value in info.items():
+    #decoded = TAGS.get(tag, tag)
     #pp.pprint(decoded)
     #pp.pprint(value)
 
+def retrieve_imgur(url):
+  file_temp = tempfile.NamedTemporaryFile(
+    delete=False,
+    mode='w+b',
+    prefix='rmg-imgur',
+    suffix='.jpg',
+  )
+  print(file_temp.name)
+  m = re.search('^.*\/(\S+?)$', url)
+  img = imgur.get_image(m.group(1))
+  #pp.pprint(inspect.getmembers(img))
+  url_img = img.link
+  urllib.request.urlretrieve(url_img, file_temp.name)
+  return file_temp
 
+def retrieve_reddit(url):
+  file_temp = tempfile.NamedTemporaryFile(
+    delete=False,
+    mode='w+b',
+    prefix='rmg-reddit-',
+    suffix='.jpg',
+  )
+  print(file_temp.name)
+  urllib.request.urlretrieve(url, file_temp.name)
+  return file_temp
 
 for submission in subreddit.stream.submissions():
-  pp.pprint(inspect.getmembers(submission))
+  #pp.pprint(inspect.getmembers(submission))
   if submission.preview:
     #pp.pprint(submission.preview['images'][0]['resolutions'][-1]['url'])
     #url = submission.preview['images'][0]['resolutions'][-1]['url']
-    # check for imgur here
-    url = submission.url
-    retrieve(url)
+    url_preview_reddit = submission.preview['images'][0]['resolutions'][0]['url'] 
+    print(url_preview_reddit)
+    file_reddit = retrieve_reddit(url_preview_reddit)
+    url_imgur = submission.url
+    file_imgur = retrieve_imgur(url_imgur)
+    getexif(file_reddit)
     print(submission.title)
---
+++ test-rmg.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+
+  def test_upper(self):
+    self.assertEqual('foo'.upper(), 'FOO')
+
+if __name__ == '__main__':
+  unittest.main()
