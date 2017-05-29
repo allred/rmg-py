bf77a4086c25eb76e34775a61f7d461a377710c0
Mike Allred <mikejallred@gmail.com>
2017-05-28 21:30:20 -0400

    Starting on (re-building) submitter.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # rmg-py
 RMG
+
+## ideas
+
+1. allow flickr for uploads to get exif gps data (if geo is feasible) 
--- listen.py
+++ listen.py
@@ -7,18 +7,19 @@ from base import *
 def getexif(file_temp):
   i = Image.open(file_temp.name)
   # FIXME: imgur strips exif, get exif from the smallest reddit thumbnail
-  info = i._getexif()
-  #pp.pprint(inspect.getmembers(info))
-  #for tag, value in info.items():
-    #decoded = TAGS.get(tag, tag)
-    #pp.pprint(decoded)
-    #pp.pprint(value)
+  exif = i._getexif()
+  #pp.pprint(inspect.getmembers(exif))
+  for tag, value in exif.items():
+    decoded = TAGS.get(tag, tag)
+    pp.pprint(decoded)
+    pp.pprint(value)
+  return exif 
 
 def retrieve_imgur(url):
   file_temp = tempfile.NamedTemporaryFile(
     delete=False,
     mode='w+b',
-    prefix='rmg-imgur',
+    prefix='rmg-imgur-',
     suffix='.jpg',
   )
   print(file_temp.name)
@@ -42,13 +43,16 @@ def retrieve_reddit(url):
 
 for submission in subreddit.stream.submissions():
   #pp.pprint(inspect.getmembers(submission))
+  #pp.pprint(submission.thumbnail)
   if submission.preview:
     #pp.pprint(submission.preview['images'][0]['resolutions'][-1]['url'])
     #url = submission.preview['images'][0]['resolutions'][-1]['url']
+    #url_preview_reddit = submission.preview['images'][0]['resolutions'][0]['url'] 
     url_preview_reddit = submission.preview['images'][0]['resolutions'][0]['url'] 
     print(url_preview_reddit)
     file_reddit = retrieve_reddit(url_preview_reddit)
     url_imgur = submission.url
     file_imgur = retrieve_imgur(url_imgur)
-    getexif(file_reddit)
+    #info = getexif(file_reddit)
+    #info = getexif(file_reddit)
     print(submission.title)
